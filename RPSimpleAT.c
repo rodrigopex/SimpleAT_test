c5d873f38c82f8426446ca250092866c2d7992fc
rodrigopex <rodrigopex@gmail.com>
2017-08-30 18:32:02 -0300

    Parameter working Signed-off-by: rodrigopex <rodrigopex@gmail.com>

--- RPSimpleAT.c
+++ RPSimpleAT.c
@@ -18,13 +18,70 @@ static uint8_t (*__available)(void);
 /*AT+READ3=HHHH<CR>*/
 /*AT+READ4=HHHH<CR>*/
 
+uint8_t asciiToUint8(uint8_t character) {
+    switch (character) {
+    case '0': return 0;
+    case '1': return 1;
+    case '2': return 2;
+    case '3': return 3;
+    case '4': return 4;
+    case '5': return 5;
+    case '6': return 6;
+    case '7': return 7;
+    case '8': return 8;
+    case '9': return 9;
+    case 'a':
+    case 'A': return 0xA;
+    case 'b':
+    case 'B': return 0xB;
+    case 'c':
+    case 'C': return 0xC;
+    case 'd':
+    case 'D': return 0xD;
+    case 'e':
+    case 'E': return 0xE;
+    case 'f':
+    case 'F': return 0xF;
+    default: return 0x00;
+    }
+}
+
+uint8_t isDigit(uint8_t character) {
+    switch (character) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case 'a':
+    case 'A':
+    case 'b':
+    case 'B':
+    case 'c':
+    case 'C':
+    case 'd':
+    case 'D':
+    case 'e':
+    case 'E':
+    case 'f':
+    case 'F': return 1;
+    default: return 0;
+    }
+}
+
+
 void __stateMachineDigest(uint8_t current) {
     static uint8_t state;
-    static uint8_t cmd[50];
+    //static uint8_t cmd[50];
     static int8_t currentCommand = -1;
     static uint8_t currentCommandIndex = 0;
 
-    static uint32_t params[0] = {0};
+    static uint32_t params[10] = {0};
     static uint8_t currentParam;
     static uint8_t currentParamIndex;
 
@@ -78,7 +135,8 @@ void __stateMachineDigest(uint8_t current) {
         if(current == '\n') {
             if(currentCommandIndex == __engine[currentCommand].sizeOfCommand) {
                 if(__engine[currentCommand].numberOfArgs == 0) {
-                    printf("Call client\n!");
+                    printf("Client return must be here\n");
+                    printf("\n\nOK\n");
                     state = 0;
                 } else {
                     state = 0;
@@ -92,8 +150,11 @@ void __stateMachineDigest(uint8_t current) {
                   (__engine[currentCommand].command[currentCommandIndex] == current)) {
             currentCommandIndex++;
         } else if(currentCommandIndex == __engine[currentCommand].sizeOfCommand) {
-            if(current == '='){
+
+            if(current == '=' && __engine[currentCommand].numberOfArgs > 0){
                 state = 5;
+                currentParam = 0;
+                currentParamIndex = 0;
             } else {
                 state = 255;
             }
@@ -103,12 +164,38 @@ void __stateMachineDigest(uint8_t current) {
         break;
     }
     case 5:
-        printf("State 5\n"); // get paramenter 1
+        printf("State 5\n"); // get paramenters
+        uint8_t sizeInBytes = __engine[currentCommand].argsSize[currentParam]<<1;
+        printf("param index %d, %d, %02X\n",currentParamIndex, sizeInBytes, current);
         if(current == '\n') {
-            state = 0;
-            printf("\n\nERROR\n");
-        }
-        else
+            if(currentParamIndex == sizeInBytes && (__engine[currentCommand].numberOfArgs == currentParam + 1)) {
+                printf("Call client with params:");
+                for(int i = 0; i < __engine[currentCommand].numberOfArgs; ++i) {
+                    printf(" %04X", params[i]);
+                }
+                printf("\n");
+                printf("\n\nOK\n");
+                state = 0;
+            } else {
+                state = 0;
+                printf("\n\nERROR\n");
+            }
+        } else if(isDigit(current) && currentParamIndex < sizeInBytes) {
+            params[currentParam] |= (uint32_t) asciiToUint8(current) << (4 * (sizeInBytes - currentParamIndex - 1));
+            currentParamIndex++;
+        } else if(currentParamIndex == sizeInBytes) {
+            if(__engine[currentCommand].numberOfArgs > currentParam + 1) {
+                if(current == ','){
+                    currentParamIndex = 0;
+                    currentParam++;
+                    printf("aqui\n");
+                } else {
+                    state = 255;
+                }
+            } else {
+                state = 255;
+            }
+        }else
             state = 255; //error
         break;
     case 255:
--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include "RPSimpleAT.h"
 #include "Stub.h"
 
-/*AT_ENGINE_START(atEngine);
+/*
+AT_ENGINE_START(atEngine);
 AT_COMMAND_2ARGS("WRITE", AT_ARGS_LIST(ADDR,VALUE), 4, 2,  writeClient);
 AT_COMMAND_1ARG("READ1", AT_ARGS_LIST(AT_ARG(ADDR, 2)), read1Client);
 AT_COMMAND("READ2", AT_ARGS_LIST(AT_ARG(ADDR, 2)), read2Client);
 AT_COMMAND("READ3", AT_ARGS_LIST(AT_ARG(ADDR, 2)), read3Client);
 AT_COMMAND("READ4", AT_ARGS_LIST(AT_ARG(ADDR, 2)), read4Client);
 AT_ENGINE_END(atEngine);
-
 */
+
 void writeClient(uint16_t addr, uint8_t value){
     printf(__FUNCTION__);
 }
@@ -33,16 +34,18 @@ int main(int argc, char **argv) {
         printf("No commands...\n");
         return 1;
     } else {
-        printf("data: %s\n",argv[1]);
+        printf("data -> [%s]\n",argv[1]);
     }
     StubInit(argv[1]);
     RPATEngineInitDriver((void*) StubOpen, (void*) StubRead, (void*) StubWrite, (void*) StubAvailable);
     RPATCommandDescriptor atEngine[] = {
         {"WRITE", 5, {0}, 0, (void*) writeClient},
-        {"XWRITE", 6, {2}, 1, (void*) writeClient}
+        {"XWRITE", 6, {2}, 1, (void*) writeClient},
+        {"READ", 4, {2,1}, 2, (void*) writeClient}
     };
-    RPATEngineInit(atEngine, 2);
+    RPATEngineInit(atEngine, 3);
     while(RPATEngineRun()) {
+        //spare code
     }
 
     return 0;
